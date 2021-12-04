6a64841ad357914de76cd9d854ff37cb0aa92cb1
kjr <kjr0814@hotmail.com>
2021-12-04 11:43:36 +0900

    프래그먼크 동작 관련 코드

--- app/src/main/java/com/neppplus/fragment_20211204/fragments/FirstFragemnt.kt
+++ app/src/main/java/com/neppplus/fragment_20211204/fragments/FirstFragemnt.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.neppplus.fragment_20211204.R
 
 class FirstFragemnt : Fragment() {
 
+//    xml을 연결해주는 역할만 담당
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,6 +20,16 @@ class FirstFragemnt : Fragment() {
 
         return inflater.inflate(R.layout.fragment_first, container, false)
 
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnToast.setonClickListner {
+            Toast.makeText("프래그머크에서 토스트 띄우기")
+
+        }
 
     }
 
