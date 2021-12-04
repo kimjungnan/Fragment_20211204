b11f560dfceb98dc29269a6bd371b21a7270a583
kjr <kjr0814@hotmail.com>
2021-12-04 12:18:37 +0900

    두번째 프래그먼크 xml추가

--- app/src/main/java/com/neppplus/fragment_20211204/fragments/FirstFragemnt.kt
+++ app/src/main/java/com/neppplus/fragment_20211204/fragments/FirstFragemnt.kt
@@ -1,12 +1,14 @@
 package com.neppplus.fragment_20211204.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.neppplus.fragment_20211204.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragemnt : Fragment() {
 
@@ -26,8 +28,10 @@ class FirstFragemnt : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        btnToast.setonClickListner {
-            Toast.makeText("프래그머크에서 토스트 띄우기")
+        btnToast.setOnClickListener {
+
+            Toast.makeText(requireContext(), "토스트 띄우기", Toast.LENGTH_SHORT).show()
+            //Log.d("프래그먼트", "프래그먼트 로그")
 
         }
 
